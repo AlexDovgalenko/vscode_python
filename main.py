cd051e7db9a7f7384e3b00d095bfe83c67452536
Alex D <a.dovgalenko2@gmail.com>
2025-06-01 20:50:23 -0400

    Added dummy main file

---
+++ main.py
@@ -0,0 +1,16 @@
+def main() -> None:
+    """Main entry point for the application."""
+    print("Hello from vscode-python!  ")
+
+
+if __name__ == "__main__":
+    main()
+    # This is the main entry point for the application.
+    # It calls the main function to execute the program.
+    # You can run this script directly to see the output.
+    # If you want to run this script, use the command:
+    # python main.py
+    # You can also run this script in a Python environment or IDE.
+    # This is a simple Python script that prints a message to the console.
+    # You can modify this script to add more functionality.
+    # This is a comment explaining the purpose of the script.
